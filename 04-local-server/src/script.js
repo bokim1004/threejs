8d9bf4469ce8e019f32199870c8e5992d4841686
bokim1004 <chloe@perfitt.io>
2023-05-14 22:59:10 +0900

    add: local-server added

---
+++ 04-local-server/src/script.js
@@ -0,0 +1,37 @@
+import * as THREE from 'three'
+
+console.log(THREE)
+
+//Scene (like container)
+
+const scene = new THREE.Scene();
+
+//Red cube
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
+//sizes
+
+const sizes = {
+    width: 800,
+    height: 600,
+};
+
+//Camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.z = 3;
+
+scene.add(camera);
+
+//Renderer
+const canvas = document.querySelector(".webgl");
+
+// Three.js will render the scene in a canvas
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas,
+});
+
+renderer.setSize(sizes.width, sizes.height);
+renderer.render(scene, camera);
